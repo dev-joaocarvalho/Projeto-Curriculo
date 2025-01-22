6b8f734b6fcc8ddb20780b11fcd806de6ef466d9
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-01-22 09:45:21 -0300

    Update MeuCurriculo.py

--- MeuCurriculo.py
+++ MeuCurriculo.py
@@ -47,11 +47,13 @@ with col2:
     st.caption("Brasília, DF, 73105-905")
     st.caption("**☎️ +55 61 98199-4401**")
     st.caption("**dev.joaocarvalho@gmail.com**")
-    button_col1, button_col2 = st.columns([1, 1])
+    button_col1, button_col2, button_col3 = st.columns([1, 1, 1])
     with button_col1:
         st.link_button("🐈‍⬛Github", "https://github.com/dev-joaocarvalho") 
     with button_col2:
         st.link_button("🌎LinkedIn", "https://www.linkedin.com/in/joaom-s-carvalho/")
+    with button_col3:
+        st.link_button("📩 E-mail", "dev.joaocarvalho@gmail.com")
         
     st.divider()
     st.write(":blue[COMPETÊNCIAS]")
